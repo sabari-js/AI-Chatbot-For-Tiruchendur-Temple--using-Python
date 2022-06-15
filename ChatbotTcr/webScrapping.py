3815d1c5b9faf717a152df9d7aca155d4238c9b8
sabari-js <95205461+sabari-js@users.noreply.github.com>
2022-06-15 22:29:02 +0530

    Add files via upload

---
+++ ChatbotTcr/webScrapping.py
@@ -0,0 +1,182 @@
+import wikipedia
+import webbrowser
+import requests
+from bs4 import BeautifulSoup
+import threading
+import smtplib
+import urllib.request
+import os
+from geopy.geocoders import Nominatim
+from geopy.distance import great_circle
+
+
+def wikiResult(query):
+	query = query.replace('wikipedia','')
+	query = query.replace('search','')
+	if len(query.split())==0: query = "wikipedia"
+	try:
+		return wikipedia.summary(query, sentences=2)
+	except Exception as e:
+		return "Desired Result Not Found"
+
+class WEATHER:
+	def __init__(self):
+		#Currently in tcr
+		self.tempValue = ''
+		self.city = ''
+		self.currCondition = ''
+		self.speakResult = ''
+
+	def updateWeather(self):
+		res = requests.get("https://ipinfo.io/")
+		data = res.json()
+		# URL = 'https://weather.com/en-IN/weather/today/l/'+data['loc']
+		URL = 'https://weather.com/en-IN/weather/today/l/e7ff0b1ec6f122f447fbff5ad04fa5bb9d882c468e1d1d53094c67817c3bc0c1'
+		result = requests.get(URL)
+		src = result.content
+
+		soup = BeautifulSoup(src, 'html.parser')
+
+		city = ""
+		for h in soup.find_all('h1'):
+			cty = h.text
+			cty = cty.replace('Weather','')
+			self.city = cty[:cty.find(',')]
+			break
+
+		spans = soup.find_all('span')
+		for span in spans:
+			try:
+				if span['data-testid']=="TemperatureValue":
+					self.tempValue = span.text[:-1]
+					break
+			except Exception as e:
+				pass
+
+		divs = soup.find_all('div', class_='CurrentConditions--phraseValue--2xXSr')
+		for div in divs:
+			self.currCondition = div.text
+			break
+
+	def weather(self):
+		from datetime import datetime
+		today = datetime.today().strftime('%A')
+		self.speakResult = "Currently in " + self.city + ", its " + self.tempValue + " degree, with a " + self.currCondition 
+		return [self.tempValue, self.currCondition, today, self.city, self.speakResult]
+
+#c = COVID()
+w = WEATHER()
+
+def dataUpdate():
+	#c.covidUpdate()
+	#c.covidUpdateIndia()
+	w.updateWeather()
+
+##### WEATHER #####
+def weather():
+	return w.weather()
+
+
+
+def latestNews(news=5):
+	URL = 'https://www.maalaimalar.com/Topic/Tiruchendur-Temple'
+	result = requests.get(URL)
+	src = result.content
+
+	soup = BeautifulSoup(src, 'html.parser')
+
+	headlineLinks = []
+	headlines = []
+
+	divs = soup.find_all('div', {'class':'col-md-4 article'},'h4')
+
+	count=0
+	for div in divs:
+		count += 1
+		if count>news:
+			break
+		a_tag = div.find('a')
+		headlineLinks.append(a_tag.attrs['href'])
+		headlines.append(a_tag.text)
+
+	return headlines,headlineLinks
+
+def maps(text):
+	text = text.replace('maps', '')
+	text = text.replace('map', '')
+	text = text.replace('google', '')
+	openWebsite('https://www.google.com/maps/place/'+text)
+
+def giveDirections(startingPoint, destinationPoint):
+
+	geolocator = Nominatim(user_agent='assistant')
+	if 'current' in startingPoint:
+		res = requests.get("https://ipinfo.io/")
+		data = res.json()
+		startinglocation = geolocator.reverse(data['loc'])
+	else:
+		startinglocation = geolocator.geocode(startingPoint)
+
+	destinationlocation = geolocator.geocode(destinationPoint)
+	startingPoint = startinglocation.address.replace(' ', '+')
+	destinationPoint = destinationlocation.address.replace(' ', '+')
+
+	openWebsite('https://www.google.co.in/maps/dir/'+startingPoint+'/'+destinationPoint+'/')
+
+	startinglocationCoordinate = (startinglocation.latitude, startinglocation.longitude)
+	destinationlocationCoordinate = (destinationlocation.latitude, destinationlocation.longitude)
+	total_distance = great_circle(startinglocationCoordinate, destinationlocationCoordinate).km #.mile
+	return str(round(total_distance, 2)) + 'KM'
+
+def openWebsite(url='https://www.google.com/'):
+	webbrowser.open(url)
+
+
+
+def youtube(query):
+	from youtube_search import YoutubeSearch
+	query = query.replace('play',' ')
+	query = query.replace('on youtube',' ')
+	query = query.replace('youtube',' ')
+	results = YoutubeSearch(query,max_results=1).to_dict()
+	webbrowser.open('https://www.youtube.com/watch?v=' + results[0]['id'])
+	return "Enjoy Sir..."
+
+
+def googleSearch(query):
+	if 'image' in query:
+		query += "&tbm=isch"
+	query = query.replace('images','')
+	query = query.replace('image','')
+	query = query.replace('search','')
+	query = query.replace('show','')
+	webbrowser.open("https://www.google.com/search?q=" + query)
+	return "Here you go..."
+
+
+
+
+def downloadImage(query, n=4):
+	query = query.replace('images','')
+	query = query.replace('image','')
+	query = query.replace('search','')
+	query = query.replace('show','')
+	URL = "https://www.google.com/search?tbm=isch&q=" + query
+	result = requests.get(URL)
+	src = result.content
+
+	soup = BeautifulSoup(src, 'html.parser')
+	imgTags = soup.find_all('img', class_='t0fcAb')
+
+	if os.path.exists('Downloads')==False:
+		os.mkdir('Downloads')
+
+	count=0
+	for i in imgTags:
+		if count==n: break
+		try:
+			urllib.request.urlretrieve(i['src'], 'Downloads/' + str(count) + '.jpg')
+			count+=1
+			print('Downloaded', count)
+		except Exception as e:
+			raise e
